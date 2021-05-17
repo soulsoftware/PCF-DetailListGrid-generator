79542ae300e5c983ce417b83f793913089eb9ab0
bsorrentino <bartolomeo.sorrentinogmail.com>
2021-05-17 13:20:13 +0200

    fix jest

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -4,13 +4,15 @@ import assert from 'yeoman-assert'
 import helpers from 'yeoman-test'
 
 describe('generator-pcf-detaillist:app', () => {
+
   beforeAll(() => {
     return helpers
-      .run(path.join(__dirname, '../generators/app'))
+      .run( path.join(__dirname, '../generators/app') )
       .withPrompts({ someAnswer: true });
   });
 
   it('creates files', () => {
     assert.file(['dummyfile.txt']);
   });
+
 });
